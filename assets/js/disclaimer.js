1d848c2a9e670334acf11d112cb11f22e0b4dd3a
Kirill Nizhnik <117679943+KirillNizhnik@users.noreply.github.com>
2024-11-20 22:35:14 +0200

    up

--- assets/js/disclaimer.js
+++ assets/js/disclaimer.js
@@ -2,8 +2,8 @@ document.addEventListener('DOMContentLoaded', function() {
     const disclaimerElement = document.getElementById('disclaimer');
 
     if (disclaimerElement) {
-        console.log('Element with ID "disclaimer" found:', disclaimerElement);
-
+        let dataContent = disclaimerElement.getAttribute('data-content');
+        console.log('Data content:', dataContent);
     } else {
         console.log('Element with ID "disclaimer" not found');
     }
