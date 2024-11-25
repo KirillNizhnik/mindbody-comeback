bf4efcd28c0c5fa005bf3c37068bd25a140381cd
Kirill Nizhnik <117679943+KirillNizhnik@users.noreply.github.com>
2024-11-25 23:46:08 +0200

    дс

--- core/mindbody/user.php
+++ core/mindbody/user.php
@@ -173,7 +173,7 @@ function hasUserActivity($user_id, $staff_token, $api_key, $site_id): bool
     }
 
     $info = getActiveClientMemberships($user_id, $staff_token, $api_key, $site_id);
-    $client_trainin = $info['ClientServices'] ;
+    $client_trainin = $info ;
     var_dump($client_trainin);
 
     if (!empty($client_trainin) && is_array($client_trainin)) {
